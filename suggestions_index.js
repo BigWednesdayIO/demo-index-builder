0d9002633e20e6309a02f3fdf9e9cb863a92f615
Chris Impey <chris@bigwednesday.io>
2015-11-03 10:48:52 +0000

    separate out product indexing and suggestions indexing

---
+++ suggestions_index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const _ = require('lodash');
+const request = require('request-promise');
+
+const indexName = 'crateful-suggestions';
+
+const stripPatterns = [
+  '\\.|,|\\*', // punctuation
+  '\\(.*\\)', // anything in brackets
+  '1.*per outlet', // 1 kit/deal per outlet, 1 per outlet
+];
+
+const removePatterns = [
+  'DELIVERY',
+  'FREE'
+];
+
+const stripRegex = new RegExp(stripPatterns.join('|'), 'gm');
+const removeRegex = new RegExp(removePatterns.join('|'), 'gm');
+
+const cleanProductName = function(name) {
+  return name
+    .replace(stripRegex, '')
+    .replace(/\s$/, '');
+};
+
+class SuggestionsIndex {
+  constructor(apiBaseUrl, apiToken) {
+    this._apiBaseUrl = `${apiBaseUrl}/indexes/${indexName}`;
+    this._apiHeaders = {
+      'content-type': 'application/json',
+      authorization: `bearer ${apiToken}`
+    }
+  }
+
+  delete() {
+    return request.del({
+      url:  this._apiBaseUrl,
+      headers: this._apiHeaders
+    })
+    .then(() => {}, err => {
+      if (err.statusCode === 404 && err.message.indexOf(indexName) >= 0) {
+        return;
+      }
+
+      throw err;
+    });
+  }
+
+  create() {
+    return request.put({
+      uri: `${this._apiBaseUrl}/settings`,
+      headers: this._apiHeaders,
+      body: {
+        searchable_fields: ['query'],
+        facet_fields: []
+      },
+      json: true
+    });
+  }
+
+  indexProductBatch(products) {
+    const requests = _(products)
+      .pluck('_source')
+      .map(source => cleanProductName(source['name']))
+      .value()
+      .filter(name => !removeRegex.test(name) && name !== 'POINT')
+      .map(name => {
+        return {action: 'create', body: {query: name}};
+      });
+
+    return request.post({
+      uri: `${this._apiBaseUrl}/batch`,
+      headers: this._apiHeaders,
+      body: {requests},
+      json: true
+    });
+  }
+}
+
+module.exports = SuggestionsIndex;
