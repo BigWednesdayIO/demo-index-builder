a261ebe6494563a0da33dd310741ea2a913532f7
toboid <toboid@users.noreply.github.com>
2015-11-03 16:51:34 +0000

    Move source specific code out of ProductsIndex class

--- products_index.js
+++ products_index.js
@@ -4,162 +4,6 @@ const _ = require('lodash');
 const request = require('request-promise');
 
 const indexName = 'crateful-products';
-const fieldsToIndex = ['name', 'brand', 'description', 'long_description'];
-
-const getCategory = (category, subcategory) => {
-  if (_.include(['AleStout', 'Lager', 'FinestCask'], category)) {
-    return {
-      code: '412.413.499676.414',
-      desc: 'Beer'
-    };
-  }
-
-  if (category === 'Wine') {
-    return {
-      code: '412.413.499676.421',
-      desc: 'Wine'
-    };
-  }
-
-  if (category === 'Spirits') {
-    if (subcategory === 'Liqueurs') {
-      return {
-        code: '412.413.499676.417.2933',
-        desc: 'Liqueurs'
-      };
-    }
-    if (subcategory === 'Vodka') {
-      return {
-        code: '412.413.499676.417.2107',
-        desc: 'Vodka'
-      };
-    }
-    if (subcategory === 'Brandy') {
-      return {
-        code: '412.413.499676.417.2364',
-        desc: 'Brandy'
-      };
-    }
-    if (subcategory === 'Sambuca') {
-      return {
-        code: '412.413.499676.417.2933',
-        desc: 'Sambuca'
-      };
-    }
-    if (_.include(['White Rum', 'Dark and Golden Rum', 'Dark & Golden Rum'])) {
-      return {
-        code: '412.413.499676.417.2605',
-        desc: 'Rum'
-      };
-    }
-    if (_.include(['Blended Whisky', 'Malt Whisky', 'Imported Whisky'])) {
-      return {
-        code: '412.413.499676.417.1926',
-        desc: '1926'
-      };
-    }
-    if (subcategory === 'Gin') {
-      return {
-        code: '412.413.499676.417.1671',
-        desc: 'Gin'
-      };
-    }
-
-    return {
-      code: '417',
-      desc: 'Liquor & Spirits'
-    };
-  }
-
-  if (category === 'SoftDrinks') {
-    if (subcategory === 'Energy Drinks') {
-      return {
-        code: '412.413.5723',
-        desc: 'Sports & Energy Drinks'
-      };
-    }
-    if (subcategory === 'Juices') {
-      return {
-        code: '412.413.2887',
-        desc: 'Juice'
-      };
-    }
-    if (subcategory === 'Mixers') {
-      return {
-        code: '412.413.499676.5725',
-        desc: 'Cocktail Mixes'
-      };
-    }
-    if (subcategory === 'Water') {
-      return {
-        code: '412.413.420',
-        desc: 'Water'
-      };
-    }
-    if (subcategory === 'Cordials') {
-      return {
-        code: '412.413.8036',
-        desc: 'Fruit-Flavoured Drinks'
-      };
-    }
-    if (subcategory === 'Canned Drinks') {
-      return {
-        code: '412.413.2628',
-        desc: 'Fizzy Drinks'
-      };
-    }
-
-    return {
-      code: '412.413',
-      desc: 'Beverages'
-    };
-  }
-
-  if (category === 'Cider') {
-    return {
-      code: '412.413.499676.6761',
-      desc: 'Cider'
-    };
-  }
-
-  if (category === 'POS') {
-    return {
-      code: '111.5863',
-      desc: 'Advertising & Marketing'
-    };
-  }
-
-  if (category === 'ReadyToDrink') {
-    return {
-      code: '412.413.499676.5887',
-      desc: 'Flavoured Alcoholic Beverages'
-    };
-  }
-
-  if (category === 'Sundries') {
-    if (subcategory === 'Co2') {
-      return {
-        code: '632.502975',
-        desc: 'Fuel Containers & Tanks'
-      };
-    }
-    if (subcategory === 'Cleaning Products') {
-      return {
-        code: '536.630.623.4973',
-        desc: 'Household Cleaning Products'
-      };
-    }
-  }
-
-  if (category === 'Other') {
-    return {
-      code: '',
-      subcategory: ''
-    };
-  }
-
-  throw new Error(`Un-mapped category/subcategory ${category}/${subcategory}`);
-};
 
 const buildCategoryHierachy = code => {
   if (!code) {
@@ -168,15 +12,6 @@ const buildCategoryHierachy = code => {
 
   const levels = code.split('.');
   return levels.map((level, index) => levels.slice(0, index + 1).join('.'));
-};
-
-const buildProduct = source => {
-  const product = _.pick(source, fieldsToIndex);
-  const category = getCategory(source.category, source.subcategory);
-  product.category_code = category.code;
-  product.category_hierachy = buildCategoryHierachy(category.code);
-  product.category_desc = category.desc;
-  return product;
 };
 
 class ProductsIndex {
@@ -214,20 +49,10 @@ class ProductsIndex {
     });
   }
 
-  indexProductBatch(products) {
-    const requests = [];
-
-    products.forEach(p => {
-      const product = buildProduct(p._source);
-      const mapToOtherBrand = !product.brand || product.brand === 'Other Brands' || product.brand.indexOf('Finest Cask Rotation') === 0;
-      product.brand = mapToOtherBrand ? 'Other' : product.brand;
-
-      for(let supplier of [{name: 'Pub Taverns', idPrefix: 'p'}, {name: 'Beer & Wine Co', idPrefix: 'b'}]) {
-        const supplierProduct = _.clone(product);
-        supplierProduct.supplier = supplier.name;
-
-        requests.push({action: 'upsert', body: supplierProduct, objectID: `${supplier.idPrefix}${p._id}`});
-      }
+  indexProductBatch(indexingRequests) {
+    const requests = indexingRequests.map(r => {
+      r.body.category_hierachy = buildCategoryHierachy(r.body.category_code);
+      return r;
     });
 
     return request.post({
